2ec21dee9d493d8591cc885a226ac362489d3259
Charbel199 <charbel-boumaroun@outlook.com>
2024-05-13 10:00:02 +0300

    Add rotation for defects in all dimensions

--- exts/defect.generation/defect/generation/ui/defects/defect_types/cracks_defect_ui.py
+++ exts/defect.generation/defect/generation/ui/defects/defect_types/cracks_defect_ui.py
@@ -12,8 +12,12 @@ class  CracksUI(BaseDefectUI):
                 "dim_w_max": self.dim_w.max_value,
                 "dim_h_min": self.dim_h.min_value,
                 "dim_h_max": self.dim_h.max_value,
-                "rot_min": self.rot.min_value,
-                "rot_max": self.rot.max_value,
+                "rot_x_min": self.rot_x.min_value,
+                "rot_x_max": self.rot_x.max_value,
+                "rot_y_min": self.rot_y.min_value,
+                "rot_y_max": self.rot_y.max_value,
+                "rot_z_min": self.rot_z.min_value,
+                "rot_z_max": self.rot_z.max_value,
                 "semantic_label": self.semantic_label.as_string,
                 "count": self._count_model.as_int}
     
@@ -26,10 +30,15 @@ class  CracksUI(BaseDefectUI):
                                   min_value=0.1,
                                   tooltip="Defining the Minimum and Maximum Length of the Defect")
 
-        self.rot = MinMaxWidget("Defect Rotation", min_value=0, max_value=360,
-                                tooltip="Defining the Minimum and Maximum Rotation of the Defect")
+        self.rot_x = MinMaxWidget("Defect Rotation X", min_value=0, max_value=360,
+                                tooltip="Defining the Minimum and Maximum X-Rotation of the Defect")
 
-
+        self.rot_y = MinMaxWidget("Defect Rotation Y", min_value=0, max_value=360,
+                        tooltip="Defining the Minimum and Maximum Y-Rotation of the Defect")
+        
+        self.rot_z = MinMaxWidget("Defect Rotation Z", min_value=0, max_value=360,
+                                tooltip="Defining the Minimum and Maximum Z-Rotation of the Defect")
+        
         with ui.HStack(height=0, tooltip="Number of defects to generate"):
             with ui.HStack():
                 ui.Label("Count", width=0)
